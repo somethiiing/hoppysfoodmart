fe039fb5ea930577890844a55795c6942168be72
Wilson Yu <wilsonweichengyu@gmail.com>
2016-10-21 17:59:31 -0700

    added social media

--- router/router.js
+++
@@ -1,9 +0,0 @@
-var router = express.Router();
-
-//api specific routes
-router.get('/', function(req, res) {
-  //send html for admin
-    res.send('Welcome to admin!');
-});
-
-exports.router = router;
--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 var express = require('express');
-var app = express();
 var path = require('path');
 var subdomain = require('express-subdomain');
-var router = require('./router/router.js')
+// var router = require('./router/router.js');
+var app = express();
+var router = express.Router();
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname + '/main/index.html'));
+// });
+
+
+app.get('*', function(req, res, next) {
+  if(req.headers.host === 'admin.localhost:8080') {
+    req.url = '/admin' + req.url;
+  }
+  next();
 });
 
+app.use('/', express.static(__dirname + '/main'));
+
+app.use('/admin', express.static(__dirname + '/admin'));
+
+
+
 app.use(subdomain('admin', router));
+
 app.listen(8080);
+
+console.log('Listening on port ' + 8080);
